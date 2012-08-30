1e7ce9c159508fc8ca0a36c8833f129b6cac3a8f
Justin Rasnic <jrasnic@fullsail.edu>
2012-08-30 13:21:34 -0400

    added more json data

--- Week1/js/json.js
+++ Week1/js/json.js
@@ -26,5 +26,157 @@ var json = {
 		"recommendation": ["Recommendation: ", "Buy"],
 		"notes": ["Notes: ", "Madden!!!"]
 	},
-
+	"game4": {
+		"gname": ["Game Name: ", "Portal"],
+		"genre": ["Genre: ", "Puzzle"],
+		"releaseDate": ["Release Date: ", "2007-10-09"],
+		"platforms": ["Platforms:", [" Windows", " Mac", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "82"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "creative"]
+	},
+	"game5": {
+		"gname": ["Game Name: ", "Limbo"],
+		"genre": ["Genre: ", "Platformer"],
+		"releaseDate": ["Release Date: ", "2010-07-01"],
+		"platforms": ["Platforms:", [" Windows", " Mac", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "78"],
+		"recommendation": ["Recommendation: ", "Rent/Borrow"],
+		"notes": ["Notes: ", "short, but good"]
+	},
+	"game6": {
+		"gname": ["Game Name: ", "Bastion"],
+		"genre": ["Genre: ", "RPG"],
+		"releaseDate": ["Release Date: ", "2011-07-01"],
+		"platforms": ["Platforms:", [" Windows", " Mac", " Xbox 360", " iOS"]],
+		"quality": ["Quality: ", "85"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "hack n slash fun"]
+	},
+	"game7": {
+		"gname": ["Game Name: ", "The Elder Scrolls V: Skyrim"],
+		"genre": ["Genre: ", "RPG"],
+		"releaseDate": ["Release Date: ", "2011-11-11"],
+		"platforms": ["Platforms:", [" Windows", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "95"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "Never ending content"]
+	},
+	"game8": {
+		"gname": ["Game Name: ", "Grand Theft Auto IV"],
+		"genre": ["Genre: ", "Action-Adventure"],
+		"releaseDate": ["Release Date: ", "2008-04-29"],
+		"platforms": ["Platforms:", [" Windows", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "80"],
+		"recommendation": ["Recommendation: ", "Rent/Borrow"],
+		"notes": ["Notes: ", "Interesting story, clunky mechanics"]
+	},
+	"game9": {
+		"gname": ["Game Name: ", "L.A. Noire"],
+		"genre": ["Genre: ", "Action-Adventure"],
+		"releaseDate": ["Release Date: ", "2011-05-17"],
+		"platforms": ["Platforms:", [" Windows", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "91"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "Outstanding storytelling, great gameplay"]
+	},
+	"game10": {
+		"gname": ["Game Name: ", "Civilization 5"],
+		"genre": ["Genre: ", "Strategy"],
+		"releaseDate": ["Release Date: ", "2010-09-21"],
+		"platforms": ["Platforms:", [" Windows", " Mac"]],
+		"quality": ["Quality: ", "83"],
+		"recommendation": ["Recommendation: ", "Rent/Borrow"],
+		"notes": ["Notes: ", "An update to te classic formula"]
+	},
+	"game11": {
+		"gname": ["Game Name: ", "Marvel Vs. Capcom 3"],
+		"genre": ["Genre: ", "Fighting"],
+		"releaseDate": ["Release Date: ", "2011-11-15"],
+		"platforms": ["Platforms:", [" Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "77"],
+		"recommendation": ["Recommendation: ", "Rent/Borrow"],
+		"notes": ["Notes: ", "fast-paced action"]
+	},
+	"game12": {
+		"gname": ["Game Name: ", "Cities XL 2012"],
+		"genre": ["Genre: ", "Simulation"],
+		"releaseDate": ["Release Date: ", "2011-10-20"],
+		"platforms": ["Platforms:", [" Windows"]],
+		"quality": ["Quality: ", "67"],
+		"recommendation": ["Recommendation: ", "Skip"],
+		"notes": ["Notes: ", "Build cities"]
+	},
+	"game13": {
+		"gname": ["Game Name: ", "StarCraft 2: Wings of Liberty"],
+		"genre": ["Genre: ", "Strategy"],
+		"releaseDate": ["Release Date: ", "2010-07-27"],
+		"platforms": ["Platforms:", [" Windows", " Mac"]],
+		"quality": ["Quality: ", "80"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "Finally!"]
+	},
+	"game14": {
+		"gname": ["Game Name: ", "Uncharted 3: Drake's Deception"],
+		"genre": ["Genre: ", "Action-Adventure"],
+		"releaseDate": ["Release Date: ", "2011-11-01"],
+		"platforms": ["Platforms:", [" Playstation 3"]],
+		"quality": ["Quality: ", "96"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "Amazing cinematic experience"]
+	},
+	"game15": {
+		"gname": ["Game Name: ", "Angry Birds"],
+		"genre": ["Genre: ", "Puzzle"],
+		"releaseDate": ["Release Date: ", "2009-12-11"],
+		"platforms": ["Platforms:", [" Windows", " Mac", " Playstation 3", " iOS"]],
+		"quality": ["Quality: ", "89"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "addictive fun"]
+	},
+	"game16": {
+		"gname": ["Game Name: ", "Amnesia: The Dark Descent"],
+		"genre": ["Genre: ", "Action-Adventure"],
+		"releaseDate": ["Release Date: ", "2008-09-08"],
+		"platforms": ["Platforms:", [" Windows", " Mac"]],
+		"quality": ["Quality: ", "89"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "survival horror; multiple endings"]
+	},
+	"game17": {
+		"gname": ["Game Name: ", "Words With Friends"],
+		"genre": ["Genre: ", "Puzzle"],
+		"releaseDate": ["Release Date: ", "2009-07-01"],
+		"platforms": ["Platforms:", [" iOS", " Android"]],
+		"quality": ["Quality: ", "82"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "make words...with friends"]
+	},
+	"game18": {
+		"gname": ["Game Name: ", "Batman: Arkham City"],
+		"genre": ["Genre: ", "Action-Adventure"],
+		"releaseDate": ["Release Date: ", "2011-10-18"],
+		"platforms": ["Platforms:", [" Windows", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "90"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "good, superhero fun"]
+	},
+	"game19": {
+		"gname": ["Game Name: ", "Blacklight: Retribution"],
+		"genre": ["Genre: ", "FPS"],
+		"releaseDate": ["Release Date: ", "2012-04-23"],
+		"platforms": ["Platforms:", [" Windows"]],
+		"quality": ["Quality: ", "81"],
+		"recommendation": ["Recommendation: ", "Buy"],
+		"notes": ["Notes: ", "Free to play!"]
+	},
+	"game20": {
+		"gname": ["Game Name: ", "Half-Life 2"],
+		"genre": ["Genre: ", "FPS"],
+		"releaseDate": ["Release Date: ", "2004-11-16"],
+		"platforms": ["Platforms:", [" Windows", " Mac", " Playstation 3", " Xbox 360"]],
+		"quality": ["Quality: ", "75"],
+		"recommendation": ["Recommendation: ", "Rent/Borrow"],
+		"notes": ["Notes: ", "Intense shooter"]
+	},
 };
